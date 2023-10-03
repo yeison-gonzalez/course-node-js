8be6a55643421d429b3770769540c794729a9b7b
YeisonGL <ydgonzalez@ccxc.us>
2023-10-03 17:06:31 -0500

    [FIX] fix model database from project

--- course-5/models/database/movie.js
+++ course-5/models/database/movie.js
@@ -17,10 +17,9 @@ export class MovieModel {
         const [genres] = await connection.query('SELECT * FROM genre WHERE LOWER(name) = ?;', [lowerCaseGenre])
 
         if (genres.length === 0) return []
-
         const [{ id }] = genres
-
-        return []
+        const [movies] = await connection.query('SELECT BIN_TO_UUID(movie.id) id, movie.title, movie.year, movie.director, movie.duration, movie.poster, movie.rate, genre.id AS genre_id, genre.name AS genre_name FROM movie JOIN movie_genres ON movie.id = movie_genres.movie_id JOIN genre ON movie_genres.genre_id = genre.id WHERE genre.id = ?;', [id]);
+        return movies
     }
 
     const [movies] = await connection.query('SELECT BIN_TO_UUID(id) id, title, year, director, duration, poster, rate FROM movie;')
@@ -40,11 +39,17 @@ export class MovieModel {
         director,
         rate,
         poster,
+        genre,
     } = input;
 
     const [resultUuid] = await connection.query('SELECT UUID() uuid;')
     const [{ uuid }] = resultUuid
     try {
+        genre?.map(async g => {
+          const [genres] = await connection.query('SELECT id, name FROM genre WHERE LOWER(name) = ?;', [g.toLowerCase()])
+          const [{ id }] = genres
+          await connection.query('INSERT INTO movie_genres (movie_id, genre_id) VALUES (UUID_TO_BIN(?), ?);', [uuid, id])
+        })
         await connection.query('INSERT INTO movie (id, title, year, director, duration, poster, rate) VALUES (UUID_TO_BIN(?), ?, ?, ?, ?, ?, ?);', [uuid, title, year, director, duration, poster, rate])
     } catch (e) {
         throw new Error('Error creating movie')
@@ -54,9 +59,35 @@ export class MovieModel {
   }
 
   static async delete({ id }) {
-    
+    const [movie] = await connection.query('SELECT BIN_TO_UUID(id) id, title, year, director, duration, poster, rate FROM movie WHERE id = UUID_TO_BIN(?);', [id])
+    if (movie) {
+      await connection.query('DELETE movie, movie_genres FROM movie JOIN movie_genres ON movie.id = movie_genres.movie_id WHERE id = UUID_TO_BIN(?);', [id]);
+      const [movies] = await connection.query('SELECT BIN_TO_UUID(id) id, title, year, director, duration, poster, rate FROM movie WHERE id = UUID_TO_BIN(?);', [id])
+      return movies.length !== 0 ? null : true;
+    } else {
+      return null;
+    }
   }
 
   static async update({ id, input }) {
+    const {
+      title,
+      year,
+      duration,
+      director,
+      rate,
+      poster,
+      genre,
+    } = input;
+
+    try {
+      const [movie] = await connection.query('SELECT BIN_TO_UUID(id) id, title, year, director, duration, poster, rate FROM movie WHERE id = UUID_TO_BIN(?);', [id])
+      const [currentMovie] = movie
+      await connection.query('UPDATE movie SET title = ?, year = ?, director = ?, duration = ?, poster = ?, rate = ? WHERE movie.id = UUID_TO_BIN(?)', [title || currentMovie.title, year || currentMovie.year, director || currentMovie.director, duration || currentMovie.duration, poster || currentMovie.poster, rate || currentMovie.rate, id])
+    } catch (e) {
+        throw new Error('Error updating movie')
+    }
+    const [movies] = await connection.query('SELECT BIN_TO_UUID(id) id, title, year, director, duration, poster, rate FROM movie WHERE id = UUID_TO_BIN(?);', [id])
+    return movies.length !== 0 ? movies[0] : null
   }
 }
